eedd88430cfdf837605ec77c0f1bee2a97fde2d4
Wilson Dos Santos <wilson-senx@hotmail.com>
2022-06-07 01:02:56 -0300

    :fire: Add and Remove a Task Done

--- src/components/TaskList/index.tsx
+++ src/components/TaskList/index.tsx
@@ -1,11 +1,56 @@
-import { useState } from 'react'
-
 import { PlusCircle, Trash } from 'phosphor-react';
-
-import './style.css';
+import { useState } from 'react';
 import { Transition } from '../Transition';
 
+import './style.css';
+
+interface Task {
+  id: number;
+  title: string;
+  isComplete: boolean;
+}
+
 export function TaskList() {
+
+  const [tasks, setTasks] = useState<Task[]>([]);
+  const [newTaskTitle, setNewTaskTitle] = useState('');
+
+  function handleCreateNewTask() {
+    
+    if (!newTaskTitle || /^\s*$/.test(newTaskTitle)) {
+      return;
+    }
+
+    const newTask = {
+      id: Math.floor(Math.random() * 1000),
+      title: newTaskTitle,
+      isComplete: false
+    }
+
+    setTasks(oldState => [...oldState, newTask]);
+
+    console.log(newTask)
+    
+    setNewTaskTitle('');
+  }
+
+  function handleToggleTaskDone(id: number) {
+
+    const tasksCompleted = tasks.map(task => task.id === id ? {
+      ...task,
+      isComplete: !task.isComplete
+    } : task);
+
+    setTasks(tasksCompleted);
+  }
+
+  function handleRemoveTask(id: number) {
+    // Remova uma task da listagem pelo ID
+
+    const filterTasks = tasks.filter(task => task.id !== id);
+
+    setTasks(filterTasks);
+  }
 
   return (
     <section className="task-list container">
@@ -14,8 +59,11 @@ export function TaskList() {
           <input 
             type="text" 
             placeholder="Adicione uma nova tarefa" 
+            onChange={(e) => setNewTaskTitle(e.target.value)}
+            value={newTaskTitle}
+            required
           />
-          <button type="submit" data-testid="add-task-button">
+          <button type="submit" data-testid="add-task-button" onClick={handleCreateNewTask}>
             Criar
             <PlusCircle size={24} />
           </button>
@@ -33,25 +81,32 @@ export function TaskList() {
         </div>
       </div>
 
-      <Transition />
+      {/*<Transition />*/}
 
-      {/*<main>
+      <main>
         <ul>
-          <li>
-            <div data-testid="task" >
+          {tasks.map((task) => (
+            <li key={task.id}>
+            <div className={task.isComplete ? 'completed' : ''} data-testid="task" >
               <div className='checkbox-container' >
-                <input type="checkbox" id="checkbox" />
+                <input 
+                  type="checkbox" 
+                  id="checkbox" 
+                  checked={task.isComplete}
+                  onClick={() => handleToggleTaskDone(task.id)}
+                />
                 <label htmlFor="checkbox"></label>
               </div>
-              <p>Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.</p>
+              <p>{task.title}</p>
             </div>
 
-            <button type="button" data-testid="remove-task-button">
+            <button type="button" data-testid="remove-task-button" onClick={() => handleRemoveTask(task.id)}>
               <Trash size={24} />
             </button>
           </li>
+          ))}
         </ul>
-      </main>*/}
+      </main>
     </section>
   )
 }
